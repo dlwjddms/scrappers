10aacdca6a249c5de59f6f5a1fcec7696252448d
dlwjddms <jeongeun3.24lee@gmail.com>
2021-03-26 09:48:50 -0700

    make url checker by go routine

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 // we will going to build URL checker
@@ -14,6 +13,10 @@ import (
  * and check each URL
  * if URL is up we willl going to say OK or FAIL
  */
+type rquestResult struct {
+	url    string
+	status string
+}
 
 var errRequestFailed = errors.New("Request Failed")
 
@@ -23,7 +26,8 @@ func main() {
 	// var results = map[string]string{}
 	// -> or use make (makes a make for me)
 	*/
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan rquestResult)
 	/* we want to do this job all at once */
 	urls := []string{
 		"https://www.airbnb.com/",
@@ -34,73 +38,29 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK" //default
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
 	}
 
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
 }
 
 /*function that will hit the websites*/
-func hitURL(url string) error {
-	/*we have to make a request for hit */
-	fmt.Println("Checking: ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		/*status code 100,200,300 redirection 400 things are wrong*/
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
-}
-
-/*
- * The way to optimize things in GO is by doing concurrency
- * Goroutines : functions do same time with other functions
- *
- * go do things like make child thread I think..
- * so main func finish go also dies
- * -> how do we communication with main func and Goroutine ?
- * = Channel(how do we communicate with goroutine)
- * main funciton store the result
- *
- * goroutine send message to main fucnction
+/*Receive from the channel,sometime you want to specify
+* This func have channel, but this channel you can not send, you can just receive
+* "c chan<- result" : send only  so "fmt.Println(<-c)"" won't work
  */
-func sexy() {
-	/* what information you will going to send? */
-	channel := make(chan string)
-	people := [2]string{"nico", "JE"}
-	for _, person := range people {
-		go isSexy(person, channel)
+func hitURL(url string, c chan<- rquestResult) {
+	/*we have to make a request for hit */
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
 	}
-	/* recieve message from channel, main func wait until he gets one message
-	   if he get ine mesg he starts again  */
-	//fmt.Println(<-channel)
-	/* now we xan receive two msg, go knows how many goroutines are running */
-	//fmt.Println(<-channel)
-	/*but if people size got bigger? -> use for loops*/
-	for i := 0; i < len(people); i++ {
-		/*recieving msg is a blocking operation*/
-		fmt.Println((<-channel))
-	}
-	// go sexyCount("nico")
-	// go sexyCount("JE")
-	//time.Sleep(time.Second * 5)
-}
-
-/* channel , the function is having one channel to communicate with main func */
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
-}
-
-/* goroutine */
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+	c <- rquestResult{url: url, status: status}
 }
